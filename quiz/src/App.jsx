e82411f5eefa89a435507b22e3e4fb12b51f202d
akhilk49 <akhilkkurian49@gmail.com>
2024-02-02 10:20:15 +0530

    ok

---
+++ quiz/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Quiz from './component/QuizComponent'
+import Result from './component/ResultComponent'
+import Home from './component/HomeComponent'
+
+
+function App() {
+
+
+  return (
+    <>
+    <Home/>
+    <Quiz/>
+    <Result/>
+     
+    </>
+  )
+}
+
+export default App
